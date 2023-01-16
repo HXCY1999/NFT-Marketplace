73f3c256d806d4e80a75cd9036a5b42a40fceedb
Haixin <112450639+HXCY1999@users.noreply.github.com>
2023-01-16 15:03:13 -0600

    2333333333333

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,4 @@
 
+export {default as Navbar } from "./navbar";
 
-
- export {default as Navbar } from "./navbar";
+export {default as BaseLayout} from "./layout/BaseLayout"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ import {Navbar} from '../components'
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <>
-      <Navbar/>
       <Component {...pageProps}/>
       </>
   )
